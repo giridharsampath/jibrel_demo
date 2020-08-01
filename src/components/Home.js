05e47b81f9208aacaed441f43b32a7c0b6af7c46
Giridhar Sampathkumar <giridharsk@Giridhars-MacBook-Pro.local>
2020-08-01 11:25:46 +0530

    added web3

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,11 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { changeText } from "../actions/appActions";
 import styled from "styled-components";
+const Web3 = require("web3");
+const PROVIDER = new Web3.providers.HttpProvider(
+  "https://mainnet.infura.io/v3/6f8e7aa0b44943fd815f439b58334b67"
+);
+const web3 = new Web3(PROVIDER);
 
 const Styles = styled.div`
   .homediv {
@@ -11,8 +16,16 @@ const Styles = styled.div`
 `;
 
 const Home = ({ appState, changeText }) => {
-  const changeWithReducer = () => {
-    changeText();
+  const changeWithReducer = async () => {
+    const response = await web3.eth.getPastLogs({
+      fromBlock: "latest",
+      toBlock: "latest",
+      address: ["0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413"],
+      topics: [
+        "0xbcfc1ebc0df4a33c12c52256d42b59221c8eaf52c6d6382c4fba6914a556938f",
+      ],
+    });
+    console.log(response);
   };
   return (
     <Styles>
@@ -21,7 +34,7 @@ const Home = ({ appState, changeText }) => {
         <br />
         <br />
         <div className="btn btn-success" onClick={changeWithReducer}>
-          Change Text
+          Fetch Data
         </div>
       </div>
     </Styles>
